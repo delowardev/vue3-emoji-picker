af2f67faeceeaefe3f526fa6d4a83b65695250ab
Delowar Hossain <delowardev@gmail.com>
2021-11-17 22:09:55 +0600

    Removing lazy load for images

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="v3-body">
     <div class="v3-body-inner" ref="bodyInner">
-      <div v-if="Object.keys(emojis).length" class="v3-group" v-for="(group, key) in emojis" :id="key" :key="key">
+      <div
+          v-if="Object.keys(emojis).length"
+          class="v3-group"
+          v-for="(group, key) in emojis"
+          :id="key"
+          :key="key"
+      >
         <h5 v-if="hasGroupNames">{{ GROUP_NAMES[key] }}</h5>
         <div class="v3-emojis">
           <button
@@ -16,7 +22,6 @@
 
             <!-- Load from CDN when options.native = true -->
             <img
-                loading="lazy"
                 v-else
                 @error="handleError($event, emoji.r)"
                 :src="EMOJI_REMOTE_SRC + `/${emoji.r}.png`"
