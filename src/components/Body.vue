025275bc4f124aa22df8121192ab0d5b567b9264
Maarten Coppens <martinuske@gmail.com>
2022-10-19 15:30:49 -0300

    use `toRaw` to define the constant, preserve `readonly`

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -49,6 +49,7 @@ import {
   computed,
   getCurrentInstance,
   inject,
+  toRaw,
 } from 'vue'
 
 /**
@@ -109,7 +110,7 @@ export default defineComponent({
     const _this = getCurrentInstance()
     const hasGroupNames = computed(() => !state.options.hideGroupNames)
     const isSticky = computed(() => !state.options.disableStickyGroupNames)
-    const groupNames = state.options.groupNames
+    const groupNames = toRaw(state.options.groupNames)
 
     if (state.options.additionalGroups) {
       Object.keys(state.options.additionalGroups).map((k) => {
